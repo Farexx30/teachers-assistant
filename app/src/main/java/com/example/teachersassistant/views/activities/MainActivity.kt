cc368cdf5fbdf9d260a809a5bd38a323b6bcb976
Łukasz Karwacki <lkarwacki3@interia.pl>
2024-12-12 23:46:37 +0100

    Feature: Part of views implemented in XML

--- app/src/main/java/com/example/teachersassistant/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/teachersassistant/views/activities/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.teachersassistant.R
+import com.example.teachersassistant.views.fragments.InitialFragment
+import com.example.teachersassistant.views.fragments.LoginFragment
+import com.example.teachersassistant.views.fragments.RegistrationFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +21,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace<InitialFragment>(R.id.fragment_container)
+            }
         }
     }
 }
